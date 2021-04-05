69bc8c5fc9b2e9a74f19ccdf48af341dbafd315e
Samir Mishra <smir.mishra1551@gmail.com>
2021-04-05 22:23:19 +0545

    files created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './jsx/home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/jsx/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/home.css';
+
+export default function Home(){
+    return(
+        <div className="home-main-container">
+            
+        </div>
+    );
+}
